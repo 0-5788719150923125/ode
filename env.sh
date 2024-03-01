93b9a235c107cc292b3170a7c584cdf8a8f0bc62
Luciferian Ink <LuciferianInk@protonmail.com>
2024-03-01 17:49:36 -0600

    nit

--- env.sh
+++
@@ -1,3 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/crow/.local/lib/python3.10/site-packages/nvidia/cuda_runtime/lib:/home/crow/.local/lib/python3.10/site-packages/nvidia/cublas/lib:/data/documents/AI-Horde-Worker-main/conda/pkgs/cudatoolkit-11.8.0-h4ba93d1_12/lib
-
-export CUDA_VISIBLE_DEVICES=0
--- venv.sh
+++ venv.sh
@@ -6,4 +6,8 @@ chmod +x env/bin/activate
 
 python3 -m pip install tensorflow[and-cuda]
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/crow/.local/lib/python3.10/site-packages/nvidia/cuda_runtime/lib:/home/crow/.local/lib/python3.10/site-packages/nvidia/cublas/lib:/data/documents/AI-Horde-Worker-main/conda/pkgs/cudatoolkit-11.8.0-h4ba93d1_12/lib
+
+export CUDA_VISIBLE_DEVICES=0
+
 python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
