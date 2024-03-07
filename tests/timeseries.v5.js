2870c9c658d09c0523d6c1454fe216b06a8509c4
Luciferian Ink <LuciferianInk@protonmail.com>
2024-03-07 05:22:51 -0600

    save state

--- tests/timeseries.v5.js
+++ tests/timeseries.v5.js
@@ -1,7 +1,6 @@
 import * as tf from '@tensorflow/tfjs-node-gpu'
 import { shaks13 } from '../src/data.js'
-// import pkg from '@tensorflow/tfjs-layers/dist/losses.js'
-// const { sparseCategoricalCrossentropy } = pkg
+import { preprocessData } from '../src/utils.js'
 
 const batchSize = 64
 const maxSequenceLength = 100
@@ -12,14 +11,14 @@ function createGRUModel(vocabSize, batchSize, maxSequenceLength) {
     model.add(
         tf.layers.embedding({
             inputDim: vocabSize,
-            batchInputShape: [batchSize, maxSequenceLength],
+            inputLength: maxSequenceLength,
             outputDim: 128,
             maskZero: true
         })
     )
 
     model.add(
-        tf.layers.lstm({
+        tf.layers.gru({
             units: 128,
             returnSequences: true,
             stateful: false,
@@ -27,18 +26,20 @@ function createGRUModel(vocabSize, batchSize, maxSequenceLength) {
         })
     )
 
-    // model.add(
-    //     tf.layers.lstm({
-    //         units: 256,
-    //         returnSequences: true,
-    //         stateful: true,
-    //         returnState: false
-    //     })
-    // )
+    // model.add(tf.layers.repeatVector(batchSize))
+
+    model.add(
+        tf.layers.gru({
+            units: 128,
+            returnSequences: true,
+            stateful: false,
+            returnState: false
+        })
+    )
 
     model.add(
         tf.layers.timeDistributed({
-            layer: tf.layers.dense({ units: vocabSize, activation: 'softmax' }) // Predicting the probability distribution over the vocabulary
+            layer: tf.layers.dense({ units: vocabSize, activation: 'softmax' })
         })
     )
 
@@ -52,93 +53,22 @@ const vocab = Array.from(
 )
 
 // Create and compile the model
-const model = createGRUModel(vocab.length, batchSize, maxSequenceLength)
+const model = createGRUModel(vocab.length, batchSize, maxSequenceLength / 2)
 model.compile({ optimizer: 'rmsprop', loss: 'categoricalCrossentropy' })
 
 console.log(model.summary())
 
-const { input, output } = sliceStringWithOverlap(shaks13, maxSequenceLength)
-
-// function sliceString(inputString, length) {
-//     const input = []
-//     const output = []
-
-//     for (let i = 0; i < inputString.length - length; i++) {
-//         input.push(inputString.slice(i, i + length))
-//         output.push(inputString.slice(i + 1, i + length + 1))
-//     }
-
-//     return { input, output }
-// }
-
-function sliceStringWithOverlap(inputString, length) {
-    const input = []
-    const output = []
-    const step = Math.floor(length / 2)
-
-    for (let i = 0; i < inputString.length - length; i += step) {
-        const inputSlice = inputString.slice(i, i + length)
-        const outputSlice = inputString.slice(i + step, i + step + length)
-
-        input.push(inputSlice)
-        output.push(outputSlice)
-    }
-
-    return { input, output }
-}
-
-function preprocessData(texts, vocab, maxSequenceLength, paddingSide = 'left') {
-    return texts.map((text) => {
-        const chars = text.split('')
-        const indices = chars.map((char) => vocab.indexOf(char))
-        const padding = new Array(maxSequenceLength - indices.length).fill(0)
-        while (indices.length < maxSequenceLength) {
-            if (paddingSide === 'left') {
-                return padding.concat(indices)
-            } else if (paddingSide === 'right') {
-                return indices.concat(padding)
-            } else {
-                if (Math.random() < 0.5) {
-                    indices.push(0)
-                } else {
-                    indices.unshift(0)
-                }
-            }
-            return indices
-        }
-        return indices
-    })
-}
-console.log(input.slice(0, 5))
-console.log(output.slice(0, 5))
-const inputIndices = preprocessData(input, vocab, maxSequenceLength, 'left')
-const outputIndices = preprocessData(output, vocab, maxSequenceLength, 'left')
-// console.log(inputIndices.slice(0, 3))
-// console.log(outputIndices.slice(0, 3))
-
-const xTensor = tf.tensor2d(inputIndices, [
-    inputIndices.length,
-    maxSequenceLength
-])
-
-const ySequences = outputIndices.map((sequence) =>
-    tf.oneHot(tf.tensor1d(sequence, 'int32'), vocab.length)
+const dataGenerator = stringSampler(maxSequenceLength, shaks13)
+const dataset = tf.data.generator(
+    createBatchGenerator(dataGenerator, vocab, batchSize, maxSequenceLength)
 )
-
-// Then, we stack these tensors together to create a single 3D tensor
-// The shape of the tensor will be [numSequences, sequenceLength, vocabSize]
-const yTensor = tf.stack(ySequences)
-
-console.log(xTensor)
-console.log(yTensor)
 
 async function trainModel() {
     let step = 0
-    await model.fit(xTensor, yTensor, {
-        epochs: 10000,
-        verbose: 1,
+    await model.fitDataset(dataset, {
+        epochs: 1,
+        verbose: 0,
         batchSize: batchSize,
-        shuffle: false,
         callbacks: {
             onBatchEnd: async (batch, logs) => {
                 step++
@@ -146,31 +76,28 @@ async function trainModel() {
                     console.log(`STEP=${step}, LOSS=${logs.loss}`)
                 }
                 if (step % 100 === 0) {
-                    model.resetStates()
-                    for (const text of ['a', 'b', 'c']) {
-                        const predictionInput = preprocessData(
-                            [text],
-                            0,
-                            vocab,
-                            maxSequenceLength
-                        )
-                        const predictionTensor = tf.tensor2d(predictionInput, [
-                            predictionInput.length,
-                            maxSequenceLength
-                        ])
+                    const sample = dataGenerator.next().value
+                    const textIndices = preprocessData(
+                        sample,
+                        vocab,
+                        maxSequenceLength,
+                        'left'
+                    )
+                    const xs = textIndices.slice(0, maxSequenceLength / 2)
+                    // Adjust for a single example prediction
+                    const xsTensor = tf.tensor2d(
+                        [xs],
+                        [1, maxSequenceLength / 2],
+                        'int32'
+                    ) // Note the use of [1, ...] for the shape
 
-                        const prediction = model.predict(predictionTensor)
+                    const prediction = model.predict(xsTensor)
+                    const predictedIndices = prediction.argMax(-1).dataSync()
 
-                        const predictedIndices = prediction
-                            .argMax(-1)
-                            .dataSync()
-
-                        const predictedText = Array.from(predictedIndices)
-                            .map((index) => vocab[index])
-                            .join('')
-                        console.log('Predicted text:', predictedText)
-                    }
-                    model.resetStates()
+                    const predictedText = Array.from(predictedIndices)
+                        .map((index) => vocab[index])
+                        .join('')
+                    console.log('Predicted text:', predictedText)
                 }
             }
         }
@@ -178,3 +105,72 @@ async function trainModel() {
 }
 
 trainModel()
+
+function* stringSampler(sampleLen, str = shaks13) {
+    while (true) {
+        // Generate a random start index within the string's bounds
+        const startIndex = Math.floor(Math.random() * (str.length - sampleLen))
+        // Yield a ${sampleLen} substring
+        yield str.substring(startIndex, startIndex + sampleLen)
+    }
+}
+
+function createBatchGenerator(
+    dataGenerator,
+    vocab,
+    batchSize,
+    inputLength,
+    predictLength
+) {
+    return function* () {
+        yield* batchGenerator(
+            dataGenerator,
+            vocab,
+            batchSize,
+            inputLength,
+            predictLength
+        )
+    }
+}
+
+function* batchGenerator(dataGenerator, vocab, batchSize, inputLength) {
+    while (true) {
+        let xsArray = []
+        let ysArray = []
+
+        for (let i = 0; i < batchSize; ++i) {
+            const text = dataGenerator.next().value
+            // const sample = text.slice(0, randomBetween(1, inputLength))
+            const sample = text
+
+            const textIndices = preprocessData(
+                sample,
+                vocab,
+                inputLength,
+                'left'
+            )
+
+            // create input sequence
+            const xs = textIndices.slice(0, inputLength / 2)
+
+            // predict the last character index
+            const ys = textIndices.slice(inputLength / 2, inputLength)
+
+            xsArray.push(xs)
+            ysArray.push(ys)
+        }
+
+        const xsTensor = tf.tensor2d(
+            xsArray,
+            [batchSize, inputLength / 2],
+            'int32'
+        )
+
+        const ysFlat = ysArray.flat()
+        const ysTensor = tf
+            .oneHot(tf.tensor1d(ysFlat, 'int32'), vocab.length)
+            .reshape([batchSize, inputLength / 2, vocab.length])
+
+        yield { xs: xsTensor, ys: ysTensor }
+    }
+}
