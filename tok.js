95f99eaff9ddd2486ed0060082646ef692aa6f43
Luciferian Ink <LuciferianInk@protonmail.com>
2024-03-29 11:13:35 -0500

    keep testing

--- tok.js
+++ tok.js
@@ -1,7 +1,9 @@
 import { AutoTokenizer } from '@xenova/transformers'
 ;(async function () {
-    const tokenizer = await AutoTokenizer.from_pretrained('Xenova/t5-small')
-
-    const { input_ids } = tokenizer('Hello world!')
+    const tokenizer = await AutoTokenizer.from_pretrained(
+        'Xenova/bert-base-uncased'
+    )
+    console.log(tokenizer)
+    const { input_ids } = await tokenizer('I love transformers!')
     console.log(input_ids)
 })()
