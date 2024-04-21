1a25bee1fae15955e5666bf0f53d7d718b23aff3
Luciferian Ink <LuciferianInk@protonmail.com>
2024-04-20 21:49:40 -0500

    change export

--- src/factory.js
+++ src/factory.js
@@ -1,11 +1,25 @@
+import v0 from './model.v0.js'
+import v1 from './model.v1.js'
+import v2 from './model.v2.js'
+import v3 from './model.v3.js'
+import v4 from './model.v4.js'
+import v5 from './model.v5.js'
+import v6 from './model.v6.js'
+import v7 from './model.v7.js'
+import v8 from './model.v8.js'
+import v9 from './model.v9.js'
+
+const modules = [v0, v1, v2, v3, v4, v5, v6, v7, v8, v9]
+
 export default async function loadModelVersion(args) {
     const defaults = {
         version: 3,
         ...args
     }
     try {
-        const module = await import(`./model.v${defaults.version}.js`)
-        return new module.default(args)
+        return modules[defaults.version]
+        // const module = await import(`./model.v${defaults.version}.js`)
+        // return new module.default(args)
     } catch (error) {
         console.error(
             `Failed to load model version ${defaults.version}:`,
