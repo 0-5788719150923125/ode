965fd02baf327f99bea2a4eb306f34d80220a98b
Luciferian Ink <LuciferianInk@protonmail.com>
2024-04-25 05:20:58 -0500

    save state

--- src/model.v9.js
+++ src/model.v9.js
@@ -10,7 +10,7 @@ export default class ObjectivelyDumbExample extends ODE {
         this.units = 256
         this.sourceFormat = 'image'
         this.imageSize = 512
-        this.encoderLayers = config.encoderLayers || 6
+        this.encoderLayers = config.encoderLayers || 5
     }
 
     defineTokenizer(config) {
@@ -27,51 +27,69 @@ export default class ObjectivelyDumbExample extends ODE {
         let outputs = inputs
 
         for (let i = 0; i < this.encoderLayers; i++) {
-            const stride = i % 2 === 0 ? 1 : 2
             outputs = this.tf.layers
                 .conv2d({
                     filters: this.units,
                     kernelSize: 3,
                     activation: 'swish',
                     padding: 'same',
-                    strides: stride
+                    dilationRate: 1
                 })
                 .apply(outputs)
 
             outputs = this.tf.layers
-                .globalAveragePooling2d({ dataFormat: 'channelsLast' })
-                .apply(outputs)
-
-            outputs = this.tf.layers
-                .reshape({
-                    targetShape: [1, ...outputs.shape.slice(1)]
-                })
-                .apply(outputs)
-
-            outputs = this.ode.layers
-                .SelfAttention({
-                    units: this.units,
-                    projection: this.units * 4
-                })
-                .apply(outputs)
-
-            outputs = this.ode.layers
-                .GatedLinearUnit({
-                    units: this.units,
-                    innerDim: this.units * 4,
-                    activation: 'swish'
+                .conv2d({
+                    filters: this.units,
+                    kernelSize: 1,
+                    activation: 'swish',
+                    padding: 'same'
                 })
                 .apply(outputs)
 
             outputs = this.tf.layers
-                .reshape({
-                    targetShape: [1, 1, this.units]
+                .maxPooling2d({
+                    poolSize: [2, 2],
+                    strides: [2, 2]
                 })
                 .apply(outputs)
+
+            // const originalShape = outputs.shape
+            // outputs = this.tf.layers
+            //     .reshape({
+            //         targetShape: [
+            //             originalShape[2] * originalShape[3],
+            //             originalShape[1]
+            //         ]
+            //     })
+            //     .apply(outputs)
+
+            // outputs = this.ode.layers
+            //     .SelfAttention({
+            //         units: originalShape[3],
+            //         projection: originalShape[3] * 4
+            //     })
+            //     .apply(outputs)
+
+            // outputs = this.tf.layers
+            //     .reshape({
+            //         targetShape: [
+            //             originalShape[1],
+            //             originalShape[2],
+            //             originalShape[3]
+            //         ]
+            //     })
+            //     .apply(outputs)
+
+            console.log(outputs.shape)
         }
 
         outputs = this.tf.layers
-            .globalAveragePooling2d({ dataFormat: 'channelsLast' })
+            .reshape({
+                targetShape: [
+                    1,
+                    outputs.shape[1] * outputs.shape[2] * outputs.shape[3]
+                ]
+            })
             .apply(outputs)
 
         outputs = this.tf.layers
@@ -87,12 +105,19 @@ export default class ObjectivelyDumbExample extends ODE {
             })
             .apply(outputs)
 
-        outputs = this.tf.layers
-            .dense({
-                units: this.config.contextLength,
-                activation: 'swish'
-            })
-            .apply(outputs)
+        // outputs = this.tf.layers
+        //     .dense({
+        //         units: this.units,
+        //         activation: 'swish'
+        //     })
+        //     .apply(outputs)
+
+        // outputs = this.tf.layers
+        //     .dense({
+        //         units: this.config.contextLength,
+        //         activation: 'swish'
+        //     })
+        //     .apply(outputs)
 
         outputs = this.tf.layers
             .timeDistributed({
@@ -106,3 +131,112 @@ export default class ObjectivelyDumbExample extends ODE {
         this.model = this.tf.model({ inputs, outputs })
     }
 }
+
+// import ODE from './model.v4.js'
+
+// /**
+//  * For vision-based language modeling.
+//  * @extends ODE
+//  */
+// export default class ObjectivelyDumbExample extends ODE {
+//     constructor(config) {
+//         super(config)
+//         this.units = 256
+//         this.sourceFormat = 'image'
+//         this.imageSize = 512
+//         this.encoderLayers = config.encoderLayers || 6
+//     }
+
+//     defineTokenizer(config) {
+//         this.tokenizer = this.ode.tokenizers.Text2Image({
+//             imageSize: this.imageSize
+//         })
+//     }
+
+//     defineBuild() {
+//         const inputs = this.ode.layers.input({
+//             shape: [this.imageSize, this.imageSize, 1]
+//         })
+
+//         let outputs = inputs
+
+//         for (let i = 0; i < this.encoderLayers; i++) {
+//             const stride = i % 2 === 0 ? 1 : 2
+//             outputs = this.tf.layers
+//                 .conv2d({
+//                     filters: this.units,
+//                     kernelSize: 3,
+//                     activation: 'swish',
+//                     padding: 'same',
+//                     strides: stride
+//                 })
+//                 .apply(outputs)
+
+//             outputs = this.tf.layers
+//                 .globalAveragePooling2d({ dataFormat: 'channelsLast' })
+//                 .apply(outputs)
+
+//             outputs = this.tf.layers
+//                 .reshape({
+//                     targetShape: [1, ...outputs.shape.slice(1)]
+//                 })
+//                 .apply(outputs)
+
+//             outputs = this.ode.layers
+//                 .SelfAttention({
+//                     units: this.units,
+//                     projection: this.units * 4
+//                 })
+//                 .apply(outputs)
+
+//             outputs = this.ode.layers
+//                 .GatedLinearUnit({
+//                     units: this.units,
+//                     innerDim: this.units * 4,
+//                     activation: 'swish'
+//                 })
+//                 .apply(outputs)
+
+//             outputs = this.tf.layers
+//                 .reshape({
+//                     targetShape: [1, 1, this.units]
+//                 })
+//                 .apply(outputs)
+//         }
+
+//         outputs = this.tf.layers
+//             .globalAveragePooling2d({ dataFormat: 'channelsLast' })
+//             .apply(outputs)
+
+//         outputs = this.tf.layers
+//             .dense({
+//                 units: this.config.contextLength,
+//                 activation: 'swish'
+//             })
+//             .apply(outputs)
+
+//         outputs = this.tf.layers
+//             .reshape({
+//                 targetShape: [this.config.contextLength, 1]
+//             })
+//             .apply(outputs)
+
+//         outputs = this.tf.layers
+//             .dense({
+//                 units: this.config.contextLength,
+//                 activation: 'swish'
+//             })
+//             .apply(outputs)
+
+//         outputs = this.tf.layers
+//             .timeDistributed({
+//                 layer: this.tf.layers.dense({
+//                     units: this.tokenizer.getLength(),
+//                     activation: 'linear'
+//                 })
+//             })
+//             .apply(outputs)
+
+//         this.model = this.tf.model({ inputs, outputs })
+//     }
+// }
