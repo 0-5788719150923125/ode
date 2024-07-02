c95d8a7ae39c32cb314b6333633833bef63951ef
Luciferian Ink <LuciferianInk@protonmail.com>
2024-07-02 18:12:34 -0500

    esn

--- misc/echoStateNetwork.js
+++ misc/echoStateNetwork.js
@@ -13,22 +13,11 @@ class EchoStateNetwork {
         this.outputSize = outputSize
         this.reservoirCount = reservoirCount
 
-        this.inputWeights = []
-        this.reservoirWeights = []
+        this.reservoirModels = []
         for (let i = 0; i < reservoirCount; i++) {
-            this.inputWeights.push(tf.randomNormal([reservoirSize, inputSize]))
-
-            let reservoirWeights = tf.randomUniform(
-                [reservoirSize, reservoirSize],
-                -1,
-                1
+            this.reservoirModels.push(
+                createReservoirModel(inputSize, reservoirSize, spectralRadius)
             )
-            const norm = reservoirWeights.norm().dataSync()[0]
-            reservoirWeights = reservoirWeights.mul(
-                tf.scalar(spectralRadius / norm)
-            )
-
-            this.reservoirWeights.push(reservoirWeights)
         }
 
         this.outputWeights = tf.variable(
@@ -41,27 +30,8 @@ class EchoStateNetwork {
         let reservoirStates = []
 
         for (let i = 0; i < this.reservoirCount; i++) {
-            let reservoir = tf.zeros([batchSize, this.reservoirSize])
-            for (let j = 0; j < input.shape[1]; j++) {
-                const x = input
-                    .slice([0, j], [batchSize, 1])
-                    .reshape([batchSize, this.inputSize])
-                const prevReservoir = reservoir
-                reservoir = tf.tidy(() => {
-                    const inputActivation = tf.matMul(
-                        x,
-                        this.inputWeights[i],
-                        false,
-                        true
-                    )
-                    const reservoirActivation = tf.matMul(
-                        prevReservoir,
-                        this.reservoirWeights[i]
-                    )
-                    return tf.tanh(inputActivation.add(reservoirActivation))
-                })
-            }
-            reservoirStates.push(reservoir)
+            const reservoirOutput = this.reservoirModels[i].predict(input)
+            reservoirStates.push(reservoirOutput)
         }
 
         const combinedStates = tf.concat(reservoirStates, 1)
@@ -87,6 +57,84 @@ class EchoStateNetwork {
             await tf.nextFrame()
         }
     }
+}
+
+class ReservoirCell extends tf.layers.Layer {
+    constructor(units, spectralRadius, activation = 'tanh') {
+        super({ name: 'esn' })
+        this.units = units
+        this.spectralRadius = spectralRadius
+        this.activation = activation
+    }
+
+    build(inputShape) {
+        const inputDim = inputShape[inputShape.length - 1]
+        this.kernel = this.addWeight(
+            'kernel',
+            [inputDim, this.units],
+            'float32',
+            tf.initializers.randomUniform({ minval: -1, maxval: 1 }),
+            null,
+            true
+        )
+        this.recurrentKernel = this.addWeight(
+            'recurrent_kernel',
+            [this.units, this.units],
+            'float32',
+            tf.initializers.randomUniform({ minval: -1, maxval: 1 }),
+            null,
+            true
+        )
+        this.built = true
+    }
+
+    call(inputs, states) {
+        inputs = Array.isArray(inputs) ? inputs[0] : inputs
+        let h = states[0] || tf.zeros([inputs.shape[0], this.units])
+        const output = tf.tidy(() => {
+            const newState = tf.tanh(
+                tf.add(
+                    tf.matMul(inputs, this.kernel.read()),
+                    tf.matMul(h, this.recurrentKernel.read())
+                )
+            )
+            return [newState, newState]
+        })
+        return output
+    }
+
+    getConfig() {
+        const config = super.getConfig()
+        Object.assign(config, {
+            units: this.units,
+            activation: this.activation
+        })
+        return config
+    }
+
+    get stateSize() {
+        return this.units
+    }
+}
+
+function createReservoirModel(inputSize, reservoirSize, spectralRadius) {
+    const input = tf.input({ shape: [null, inputSize] })
+    const reservoir = tf.layers
+        .rnn({
+            cell: new ReservoirCell(reservoirSize, spectralRadius),
+            returnSequences: false
+        })
+        .apply(input)
+    const model = tf.model({ inputs: input, outputs: reservoir })
+
+    const recurrentKernel = model.getWeights()[1]
+    const norm = tf.norm(recurrentKernel, 'euclidean')
+    const scaledKernel = recurrentKernel
+        .div(norm)
+        .mul(tf.scalar(spectralRadius))
+    model.setWeights([model.getWeights()[0], scaledKernel])
+
+    return model
 }
 
 // Example usage
