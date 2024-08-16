fffbb7acbfebefd328b0abc8d5f1c51b6d8e35da
Luciferian Ink <LuciferianInk@protonmail.com>
2024-08-16 02:31:11 -0500

    add laplace activation

--- src/activations.js
+++ src/activations.js
@@ -60,9 +60,38 @@ class Snake extends tf.serialization.Serializable {
 
 tf.serialization.registerClass(Snake)
 
+/**
+ * Laplace activation function
+ */
+class LaplaceActivation extends tf.serialization.Serializable {
+    /** @nocollapse */
+    static className = 'LaplaceActivation'
+
+    /**
+     * Calculate the activation function.
+     *
+     * @param x Tensor.
+     * @param mu Mean value (default: 0.707107).
+     * @param sigma Standard deviation (default: 0.282095).
+     * @returns a Tensor of the same shape as x
+     */
+    apply(x, mu = 0.707107, sigma = 0.282095) {
+        return tf.tidy(() => {
+            const adjustedInput = tf.div(
+                tf.sub(x, mu),
+                tf.mul(sigma, Math.sqrt(2.0))
+            )
+            return tf.mul(0.5, tf.add(1.0, tf.erf(adjustedInput)))
+        })
+    }
+}
+
+tf.serialization.registerClass(LaplaceActivation)
+
 const activations = {
     APTx: new APTx(),
-    Snake: new Snake()
+    Snake: new Snake(),
+    Laplace: new LaplaceActivation()
 }
 
 export default activations
