119969f289cba5bc6cca543ca685a61446178eab
Luciferian Ink <LuciferianInk@protonmail.com>
2024-08-26 07:21:02 -0500

    fix focal loss

--- src/losses.js
+++ src/losses.js
@@ -1,97 +1,48 @@
 import * as tf from '@tensorflow/tfjs'
 
+// Focal loss is used to address the issue of the class imbalance problem.
+// A modulation term applied to the Cross-Entropy loss function.
 // https://github.com/mlyg/unified-focal-loss/blob/main/loss_functions.py
-// function categoricalFocalCrossEntropy(
-//     y_true,
-//     y_pred,
-//     weights = undefined,
-//     labelSmoothing = 0,
-//     reduction = tf.Reduction.MEAN,
-//     alpha = 0.25,
-//     gamma = 2.0,
-//     fromLogits = false
-// ) {
-//     return tf.tidy(() => {
-//         // Apply label smoothing if needed
-//         if (labelSmoothing > 0) {
-//             const numClasses = tf.scalar(y_true.shape[1])
-//             y_true = tf.add(
-//                 tf.mul(y_true, tf.sub(1.0, labelSmoothing)),
-//                 tf.div(labelSmoothing, numClasses)
-//             )
-//         }
-
-//         // Calculate softmax cross-entropy loss
-//         const crossEntropy = tf.losses.softmaxCrossEntropy(
-//             y_true,
-//             y_pred,
-//             undefined,
-//             undefined,
-//             tf.Reduction.NONE,
-//             fromLogits
-//         )
-
-//         // Calculate focal loss
-//         const alpha_t = tf
-//             .mul(y_true, alpha)
-//             .add(tf.mul(tf.sub(1.0, y_true), 1.0 - alpha))
-//         const modulation_factor = tf.pow(tf.sub(1.0, tf.softmax(y_pred)), gamma)
-//         const focalLoss = tf.mul(
-//             alpha_t,
-//             tf.mul(modulation_factor, crossEntropy.expandDims(-1))
-//         )
-
-//         // Compute weighted loss with reduction
-//         return tf.losses.computeWeightedLoss(focalLoss, weights, reduction)
-//     })
-// }
-
 function categoricalFocalCrossEntropy(
-    y_true,
-    y_pred,
+    yTrue,
+    yPred,
     weights = null,
     labelSmoothing = 0,
     reduction = null,
     fromLogits = false,
-    alpha = 0.25,
-    gamma = 2.0
+    alpha = 1.0, // alpha > 0.5 penalises false negatives more than false positives
+    gamma = 2.0 // focal parameter controls degree of down-weighting of easy examples
 ) {
     return tf.tidy(() => {
         // Clip the prediction value to prevent NaN's and Inf's
         const epsilon = tf.backend().epsilon()
-        y_pred = tf.clipByValue(y_pred, epsilon, 1.0 - epsilon)
+        yPred = tf.clipByValue(yPred, epsilon, 1.0 - epsilon)
 
         // Calculate probabilities
-        if (fromLogits) y_pred = tf.softmax(y_pred)
+        if (fromLogits) yPred = tf.softmax(yPred)
 
         // Calculate cross entropy manually
-        let cross_entropy = tf.mul(tf.neg(y_true), tf.log(y_pred))
+        let crossEntropy = tf.mul(tf.neg(yTrue), tf.log(yPred))
 
         // Apply label smoothing if needed
         if (labelSmoothing > 0) {
-            const numClasses = y_true.shape[1]
-            y_true = tf.add(
-                tf.mul(y_true, tf.sub(1.0, labelSmoothing)),
+            const numClasses = yTrue.shape[1]
+            yTrue = tf.add(
+                tf.mul(yTrue, tf.sub(1.0, labelSmoothing)),
                 tf.div(labelSmoothing, numClasses)
             )
         }
 
         // Calculate focal loss
-        const alpha_t = tf
-            .mul(y_true, alpha)
-            .add(tf.mul(tf.sub(1.0, y_true), 1.0 - alpha))
-        const modulation_factor = tf.pow(tf.sub(1.0, y_pred), gamma)
-        let focal_loss = tf.mul(
-            alpha_t,
-            tf.mul(modulation_factor, cross_entropy)
-        )
+        const modulation = tf.pow(tf.sub(1.0, yPred), gamma)
+        let losses = tf.mul(alpha, tf.mul(modulation, crossEntropy))
 
         if (weights !== null) {
-            focal_loss = tf.mul(focal_loss, weights.expandDims(-1))
+            losses = tf.mul(losses, weights.expandDims(-1))
         }
 
         // Compute scalar loss with reduction
-        return tf.mean(tf.sum(focal_loss, -1))
+        return tf.mean(tf.sum(losses, -1))
     })
 }
 
